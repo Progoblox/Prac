cc359c8e36dccc01585f318188b7c63e5d0bdc40
Program <67320703+Program132@users.noreply.github.com>
2024-01-16 21:34:54 +0100

    -> defaultSpeed to global

--- config.lua
+++ config.lua
@@ -4,10 +4,6 @@ game:GetService("Players").PlayerAdded:Connect(function(player)
     local f = Instance.new("Folder", player)
     f.Name = "PracConfig"
     
-    local defaultSpeed = Instance.new("NumberValue", f)
-    defaultSpeed.Name = "defaultSpeed"
-    defaultSpeed.Value = 16
-    
     local isModerator = Instance.new("BoolValue", f)
     isModerator.Name = "isModerator"
     isModerator.Value = false
