c953648461dfbffef14fee05f4e56db72307b514
Program <67320703+Program132@users.noreply.github.com>
2024-01-17 10:42:23 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,4 +7,16 @@ game:GetService("Players").PlayerAdded:Connect(function(player)
     local isModerator = Instance.new("BoolValue", f)
     isModerator.Name = "isModerator"
     isModerator.Value = false
+
+    local warningSpeed = Instance.new("IntValue", f)
+    warningSpeed.Name = "warningSpeed"
+    warningSpeed.Value = 0
+
+    local warningHealth = Instance.new("IntValue", f)
+    warningHealth.Name = "warningHealth"
+    warningHealth.Value = 0
+
+    local warningWallhack = Instance.new("IntValue", f)
+    warningWallhack.Name = "warningWallhack"
+    warningWallhack.Value = 0
 end)
