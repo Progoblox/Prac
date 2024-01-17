788051535e7056bb942705cd4ccf9b8a0444199e
Program <67320703+Program132@users.noreply.github.com>
2024-01-17 06:56:51 +0100

    Moved verifications to server side

--- checker_speed-health.lua
+++ checker_speed-health.lua
@@ -4,21 +4,13 @@ local char = plr.Character or plr.CharacterAdded:Wait()
 local humanoid = char:FindFirstChild("Humanoid")
 
 humanoid:GetPropertyChangedSignal("WalkSpeed"):Connect(function(onWalkSpeedChanged)
-    local current = humanoid.WalkSpeed
-    local isMod = rs.plrIsModerator:InvokeServer()
-    local defaultSpeed = rs.getDefaultSpeed:InvokeServer()
-    local maxSpeed = rs.getMaxSpeed:InvokeServer()
-    if (not isMod and current > maxSpeed) or (not isMod and current < defaultSpeed) then
+    if rs.isSpeedHack:InvokeServer(humanoid.WalkSpeed) then
         rs.kick:FireServer("Possible Speed Hack")
     end
 end)
 
 humanoid:GetPropertyChangedSignal("Health"):Connect(function(onWalkSpeedChanged)
-    local current = humanoid.Health
-    local isMod = rs.plrIsModerator:InvokeServer()
-    local defaultHealth = rs.getDefaultHealth:InvokeServer()
-    local maxHealth = rs.getMaxHealth:InvokeServer()
-    if (not isMod and current > maxHealth) or (not isMod and current < defaultHealth) then
+    if rs.isSpeedHack:InvokeServer(humanoid.Health) then
         rs.kick:FireServer("Possible Health Hack")
     end
 end)
