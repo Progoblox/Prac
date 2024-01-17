b008c9cb82a1b415ac71a642396d4a47c6b07ae5
Program <67320703+Program132@users.noreply.github.com>
2024-01-17 11:05:52 +0100

    Update manager.lua

--- manager.lua
+++ manager.lua
@@ -10,6 +10,8 @@ local defaultSpeed = 16 -- the default speed
 local maxSpeed = 16 -- max speed you gave
 local defaultHealth = 100 -- the default health
 local maxHealth = 100 -- max health you have
+local maxDistanceCheckWallHack = 100 -- max distance you give to check when there is a possible wallhack
+
 
 ----------------------------------- Functions -----------------------------------
 local function resetArgumentsHumanoidPlayer(plr, target)
@@ -42,17 +44,38 @@ local function isHealthHackFunction(player, current)
 end
 isHealthHack.OnServerInvoke = isHealthHackFunction
 
+-- WallHack
+local aimingAtPlayer = rs.aimingAtPlayer -- RF
+local function aimingAtPlayerFunction(player, targetPlayer)
+    local playerHRP = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
+    local targetHRP = targetPlayer.Character and targetPlayer.Character:FindFirstChild("HumanoidRootPart")
+
+    if playerHRP and targetHRP then
+        local direction = (targetHRP.Position - playerHRP.Position).unit
+        local ray = Ray.new(playerHRP.Position, direction * maxDistanceCheckWallHack)
+        local hitPart, hitPosition = workspace:FindPartOnRay(ray, player)
+        if hitPart and hitPart:IsA("Humanoid") and hitPart.Parent == targetPlayer.Character then
+            return true
+        end
+    end
+    return false
+end)
+aimingAtPlayer.OnServerInvoke = aimingAtPlayerFunction
+
+
+
+-- Utilities for management
+
 local warnings = rs.warnings -- RE
 warnings.OnServerEvent:Connect(function(player, source)
     v = 0
     r = ""
-    
     if source == "speed" then
-        player.PracConfig.warningSpeed.Value += 1
+        player.PracConfig.warningSpeed.Value = player.PracConfig.warningSpeed.Value + 1
         v = player.PracConfig.warningSpeed.Value
         r = "Possible Speed Hack"
     elseif source == "health" then
-        player.PracConfig.warningHealth.Value += 1
+        player.PracConfig.warningHealth.Value = player.PracConfig.warningHealth.Value + 1
         v = player.PracConfig.warningHealth.Value
         r = "Possible Health Hack"
     end
